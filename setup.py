a59562b0131408ea5b1bdb779a29faf531903d21
Papan Yongmalwong <papan.yongmalwong@seamoney.com>
2025-02-01 22:35:27 +0800

    fix cannot view game when not 4 players yet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bridgepy",
-    version="0.0.3",
+    version="0.0.4",
     author="Papan Yongmalwong",
     author_email="papillonbee@gmail.com",
     description="bridgepy is a python package for playing floating bridge!",
