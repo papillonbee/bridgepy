ed4baa1e9c194d639591e11f29992766fed4fe77
Papan Yongmalwong <papan.yongmalwong@seamoney.com>
2025-04-07 01:28:08 +0800

    upgrade to v0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bridgepy",
-    version="0.0.12",
+    version="0.0.13",
     author="Papan Yongmalwong",
     author_email="papillonbee@gmail.com",
     description="bridgepy is a python package for playing floating bridge!",
